5e5b5636cf93233e3f3277d909be5d2706bf2d1f
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2026-03-05 13:58:39 -0300

    Add Apache Camel dependencies

---
+++ camel-application/src/main/java/com/br/flavioreboucassantos/example3/example3.java
@@ -0,0 +1,13 @@
+package com.br.flavioreboucassantos.example3;
+
+import org.apache.camel.CamelContext;
+
+public class example3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "Hello World!" );
+
+	}
+
+}
