fe02d62068485a8dc6499354d75d4f92a98dc8e5
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2026-03-09 14:26:22 -0300

    Adds the final pieces to the creation of the project camel-choice-from-activemq

---
+++ camel-choice-from-activemq/src/main/java/com/br/flavioreboucassantos/camel_choice_from_activemq/App.java
@@ -0,0 +1,13 @@
+package com.br.flavioreboucassantos.camel_choice_from_activemq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
