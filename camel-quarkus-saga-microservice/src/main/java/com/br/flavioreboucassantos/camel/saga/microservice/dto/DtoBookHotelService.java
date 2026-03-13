a3a8b344d0a588124f01f784630e9ab46826928d
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2026-03-13 18:22:46 -0300

    Excluding LRA Project without Documentation

--- camel-quarkus-saga-microservice/src/main/java/com/br/flavioreboucassantos/camel/saga/microservice/dto/DtoBookHotelService.java
+++
@@ -1,8 +0,0 @@
-package com.br.flavioreboucassantos.camel.saga.microservice.dto;
-
-/**
- * @author Flávio Rebouças Santos - flavioReboucasSantos@gmail.com
- */
-public final record DtoBookHotelService(long idBookHotelService, String jsonBookHotelService) {
-
-}
--- camel-quarkus-saga-microservice/src/main/java/com/br/flavioreboucassantos/camel/saga/microservice/dto/DtoBookTrip.java
+++
@@ -1,8 +0,0 @@
-package com.br.flavioreboucassantos.camel.saga.microservice.dto;
-
-/**
- * @author Flávio Rebouças Santos - flavioReboucasSantos@gmail.com
- */
-public final record DtoBookTrip(long idBookTrip, long idBookHotelService, long idBookFlightService) {
-
-}
